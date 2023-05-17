eb966d2b7f6dcf002ec96b1792269887895ed155
Amrit Pangeni <amr1tpang3n1@gmail.com>
2023-05-17 11:33:18 +0545

    codeupdated

--- connection.php
+++ connection.php
@@ -1,6 +1,17 @@
 <?php
+	require_once __DIR__ . '/vendor/autoload.php';
 
-	$conn = mysqli_connect('localhost','root','','musical_world');
+	$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+	$dotenv->load();
+
+	$host = $_ENV['AZURE_MYSQL_HOST'];
+	$dbname = $_ENV['AZURE_MYSQL_DBNAME'];
+	$port = $_ENV['AZURE_MYSQL_PORT'];
+	$flag = constant($_ENV['AZURE_MYSQL_FLAG']);
+	$username = $_ENV['AZURE_MYSQL_USERNAME'];
+	$password = $_ENV['AZURE_MYSQL_PASSWORD'];
+
+    $conn = mysqli_connect($host,$username,$password,$dbname);
 
 	if(!$conn)
 		die("Error while connecting...!").mysqli_connect_error($conn);
